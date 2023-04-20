c88b8750266e795fd5b85f532d1c120506b8497b
emerson312 <emersincordoba2022@gmail.com>
2023-04-20 18:09:14 -0500

    rick_and_morty

---
+++ src/components/Character.jsx
@@ -0,0 +1,11 @@
+function Character({characters}) {
+  return (
+    <div className="text-center p-5">
+      <h3>{characters.name}</h3>
+      <img className="img-fluid rounded-pill" src={characters.image} alt={characters.name} />
+      <p>{characters.origin.name}</p>
+    </div>
+  );
+}
+
+export default Character;
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Character from "./Character";
+
+function NavPage({page, setPage}){
+  return(
+    <header className="d-flex justify-content-between aling-items-center">
+      <p>Page: {page}</p>
+      <button className="btn btn-primary btm-sm" onClick={() => {
+        setPage(page + 1)
+      }}>
+        Page: {page + 1}
+      </button>
+    </header>
+  )
+}
+
+function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    async function fetData() {
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+      const data = await response.json();
+      setLoading(false);
+      setCharacters(data.results);
+    }
+    fetData();
+  }, [page]);
+  return (
+    <div className="container">
+      <NavPage page={page} setPage={setPage}/>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((characters) => {
+            return (
+              <div className="col-md-4" key={characters.id}>
+                <Character characters={characters} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <NavPage page={page} setPage={setPage}/>
+    </div>
+  );
+}
+
+export default CharacterList;
